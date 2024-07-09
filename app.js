f2f428474ad53680070ec31c8c0788400e7e3779
Nagusha <160119980+Nagusha@users.noreply.github.com>
2024-07-09 16:46:55 +0530

    Update app.js

--- app.js
+++ app.js
@@ -13,3 +13,75 @@ const { Author, Book, Loan, Members, Reservation } = require('./models');
     console.error('Unable to connect to the database:', error);
   }
 })();
+//CRUD operations
+//CREATE:
+const createAuthor = async (authorData) => {
+    try {
+      const author = await Author.create(authorData);
+      console.log('Author created:', author.toJSON());
+      return author;
+    } catch (error) {
+      console.error('Error creating author:', error);
+    }
+  };
+createAuthor({ name: 'Nagusha' });
+
+//Read
+const getAllAuthors = async () => {
+    try {
+      const authors = await Author.findAll();
+      console.log('All authors:', authors);
+      return authors;
+    } catch (error) {
+      console.error('Error getting authors:', error);
+    }
+  };
+
+  const getAuthorById = async (id) => {
+    try {
+      const author = await Author.findByPk(id);
+      if (author) {
+        console.log('Author found:', author.toJSON());
+        return author;
+      } else {
+        console.log('Author not found');
+      }
+    } catch (error) {
+      console.error('Error getting author:', error);
+    }
+  };
+  getAllAuthors();
+  getAuthorById(1);
+
+//UPDATE
+const updateAuthor = async (id, updateData) => {
+    try {
+      const author = await Author.findByPk(id);
+      if (author) {
+        await author.update(updateData);
+        console.log('Author updated:', author.toJSON());
+        return author;
+      } else {
+        console.log('Author not found');
+      }
+    } catch (error) {
+      console.error('Error updating author:', error);
+    }
+  };
+  updateAuthor(1, { name: 'Nagusha' });
+
+//DELETE
+const deleteAuthor = async (id) => {
+    try {
+      const author = await Author.findByPk(id);
+      if (author) {
+        await author.destroy();
+        console.log('Author deleted');
+      } else {
+        console.log('Author not found');
+      }
+    } catch (error) {
+      console.error('Error deleting author:', error);
+    }
+  };
+  deleteAuthor(1);
