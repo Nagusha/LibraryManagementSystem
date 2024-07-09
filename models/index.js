a1f08f1eceb89a6a0a83ba4d3827de09e93a70bc
Nagusha <160119980+Nagusha@users.noreply.github.com>
2024-07-09 10:19:45 +0530

    Update index.js

--- models/index.js
+++ models/index.js
@@ -1,30 +1,13 @@
-'use strict';
-
-const { Sequelize, DataTypes } = require('sequelize');
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize('database24', 'nagusha', 'intern', {
-  host: 'localhost',
-  dialect: 'postgres',
-});
-
-const models = {};
-
-fs.readdirSync('Hii')
-  .filter(file => file !== 'index.js')
-  .forEach(file => {
-    const model = require(path.join('Hii', file))(sequelize, Sequelize.DataTypes);
-    models[model.name] = model;
-  });
-
-  Object.keys(models).forEach(modelName => {
-    if ('associate' in models[modelName]) {
-        models[modelName].associate(models);
-    }
-});
+const Author = require('./author');
+const Book = require('./book');
+const Loan = require('./loan');
+const Members = require('./members');
+const Reservations = require('./reservations');
 
 module.exports = {
-  sequelize,
-  models
+  Author,
+  Book,
+  Loan,
+  Members,
+  Reservations
 };
